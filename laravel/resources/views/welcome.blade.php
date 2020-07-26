54ea78c4a1c99b475eb399b176d0b15cc834cf31
Thiago Bruno <thiago.bruno@geracaoweb.com>
2020-07-25 21:42:26 -0300

    base

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -86,14 +86,10 @@
 
                 <div class="links">
                     <a href="/api/documentation">Godot + Laravel API Reference</a>
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Laravel Docs</a>
+                    <a href="https://youtube.com.br/thiagobruno">Thiago Bruno Channel</a>
+                    <a href="https://github/thiagobruno">Thiago Bruno GitHub</a>
+                    <a href="https://picpay.me/thiagobrun0">Sponsorships</a>
                 </div>
             </div>
         </div>
