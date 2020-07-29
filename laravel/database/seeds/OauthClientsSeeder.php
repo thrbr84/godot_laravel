fb2e4009d05b44bb6de91c6b6ac471a179ca0607
Thiago Bruno <thiago.bruno@geracaoweb.com>
2020-07-29 14:30:34 -0300

    base

--- laravel/database/seeds/OauthClientsSeeder.php
+++ laravel/database/seeds/OauthClientsSeeder.php
@@ -19,15 +19,36 @@ class OauthClientsSeeder extends Seeder
         DB::table('oauth_clients')->insert([
             [
                 'id' => 1,
-                'user_id' => 1,
+                'user_id' => null,
                 'name' => 'GodotLaravel (Client)',
                 'secret' => $faker->regexify('[A-Za-z0-9]{40}'),
                 'redirect' => 'http://localhost',
-                'personal_access_client' => 0,
+                'personal_access_client' => 1,
                 'password_client' => 1,
                 'revoked' => 0,
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'id' => 2,
+                'user_id' => null,
+                'name' => 'GodotLaravel (Personal)',
+                'secret' => $faker->regexify('[A-Za-z0-9]{40}'),
+                'redirect' => 'http://localhost',
+                'personal_access_client' => 0,
+                'password_client' => 0,
+                'revoked' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ]);
+
+        // Permite criação de personal tokens
+        DB::table('oauth_personal_access_clients')->insert([
+            [
+            'id' => 1,
+            'client_id' => 1,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]
         ]);
     }
